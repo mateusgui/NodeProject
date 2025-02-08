435182d12a0385f69df825b8162874061a032a71
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-02-07 22:31:57 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 import express from 'express'
 const app = express()
+app.use(express.json())
+
+const users = []
+
+app.get('/usuarios', (request,response) => {
+
+    response.status(200).json(users)
+})
+
+app.post('/usuarios', (request,response) => {
+    users.push(request.body)
+
+    response.status(201).json({ message: "Usuário criado com sucesso!"})
+})
 
 app.listen(3000)
