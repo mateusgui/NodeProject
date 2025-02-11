b78b0d53f606e26628f8ad1984624fa302605a2d
Mateus Guimarães Moraes Vilela <mateusguimaraesmoraes14@gmail.com>
2025-02-10 20:40:30 -0400

    API finalizada

--- server.js
+++ server.js
@@ -1,18 +1,69 @@
+//EXPRESS
 import express from 'express'
 const app = express()
 app.use(express.json())
+app.listen(3000) // http://localhost:3000
 
-const users = []
+//PRISMA
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
-app.get('/usuarios', (request,response) => {
 
-    response.status(200).json(users)
+//GET - LISTAR TODOS - http://localhost:3000/usuarios
+app.get('/usuarios', async (request,response) => {
+
+    const user = await prisma.user.findMany()
+
+    response.status(200).json(user)
 })
 
-app.post('/usuarios', (request,response) => {
-    users.push(request.body)
 
-    response.status(201).json({ message: "Usuário criado com sucesso!"})
+//POST - CRIAR - http://localhost:3000/usuarios
+app.post('/usuarios', async (request,response) => {
+    
+    const user = await prisma.user.create({
+        data: {
+            email: request.body.email,
+            name: request.body.name,
+            age: request.body.age
+        }
+    })
+
+    response.status(201).json(user)
 })
 
-app.listen(3000)
+
+//PUT - ATUALIZAR pelo ID http://localhost:3000/usuarios/ID
+app.put('/usuarios/:id', async (request,response) => {
+    
+    const user = await prisma.user.update({
+        where: {
+            id: request.params.id
+        },
+        data: {
+            email: request.body.email,
+            name: request.body.name,
+            age: request.body.age
+        }
+    })
+
+    response.status(200).json(user)
+})
+
+
+//DELETE - DELETAR pelo ID http://localhost:3000/usuarios/ID
+app.delete('/usuarios/:id', async (request,response) => {
+    
+    const user = await prisma.user.delete({
+        where: {
+            id: request.params.id
+        }
+    })
+        
+    response.status(200).json({message: 'Usuario deletado com sucesso!'})
+})
+
+
+
+
+
